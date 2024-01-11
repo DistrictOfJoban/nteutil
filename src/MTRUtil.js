9ef8422b6e53aae715104ed725cb5e4ada1a96dd
LX862 <kenny.mh.hui@outlook.com>
2024-01-12 00:10:07 +0800

    Add getClosePlatform

--- src/MTRUtil.js
+++ src/MTRUtil.js
@@ -19,5 +19,22 @@ const MTRUtil = {
     },
     getConnectingStation(stnId) {
         return MTRClientData.DATA_CACHE.stationIdToConnectingStations.get(stnId);
+    },
+    getClosePlatform(pos, radius, lower, upper) {
+        if(radius == null) radius = 5;
+        if(lower == null) lower = 0;
+        if(upper == null) upper = 4;
+        let platforms = MTRClientData.PLATFORMS;
+        let dataCache = MTRClientData.DATA_CACHE;
+
+        let platform = 0;
+        for (let i = 1; i <= radius; i++) {
+            let searchRadius = i;
+            platform = platforms.stream().filter(platform => platform.isCloseToSavedRail(pos, searchRadius, lower, upper)).min(java.util.Comparator.comparingInt(platform => platform.getMidPos().distManhattan(pos))).orElse(null);
+            if (platform != null) {
+                break;
+            }
+        }
+        return platform;
     }
 }
